7442eacc4f92fab539d2f58a7543a53818317e5a
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-11-09 07:33:38 -0700

    Added concept of Epoch Time to SystemTime implementation.

--- src/proj/NanoMessageBus/SystemTime.cs
+++ src/proj/NanoMessageBus/SystemTime.cs
@@ -21,5 +21,30 @@ namespace NanoMessageBus
 		{
 			get { return Resolver == null ? DateTime.UtcNow : Resolver(); }
 		}
+
+		/// <summary>
+		/// Gets the number of seconds that have elapsed between the instant and Unix Epoch Time (12:00 AM January 1, 1970).
+		/// </summary>
+		/// <param name="instant">The instant from which epoch time will be computed.</param>
+		/// <returns>The number of seconds that have elapsed since the instant provided.</returns>
+		public static long ToEpochTime(this DateTime instant)
+		{
+			return (long)(instant - EpochTime).TotalSeconds;
+		}
+
+		/// <summary>
+		/// Gets the point in time represented by the instant specified.
+		/// </summary>
+		/// <param name="epochTime">The point in time, according to Unix Epoch Time to be converted.</param>
+		/// <returns>The point in time expressed as a DateTime.</returns>
+		public static DateTime ToDateTime(this long epochTime)
+		{
+			return EpochTime + TimeSpan.FromSeconds(epochTime);
+		}
+
+		/// <summary>
+		/// Represents "zero" for Unix Epoch Time.
+		/// </summary>
+		public static readonly DateTime EpochTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 	}
 }
