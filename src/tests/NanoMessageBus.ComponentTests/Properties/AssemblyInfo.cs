490e1ce1e309d8c93959bb05b85253d97c9d3ce7
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-11-14 05:42:32 -0700

    Adding component and unit tests for core project.

---
+++ src/tests/NanoMessageBus.ComponentTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NanoMessageBus.ComponentTests")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("4b858332-4268-47ff-b924-021b9e0d5c71")]
---
+++ src/tests/NanoMessageBus.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NanoMessageBus.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("368eedba-1412-40a9-9fac-8e6b37b91be1")]
