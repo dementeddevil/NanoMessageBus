d75d0246f04d2a52fb33661487bd9eca90718e71
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-11-14 19:42:40 -0700

    Acceptance tests are about the target audience (the business) more than being isolated into a separate project.

--- src/tests/NanoMessageBus.RabbitChannel.AcceptanceTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NanoMessageBus.RabbitChannel.AcceptanceTests")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("758fee5b-4d65-4fab-b21d-a9f28e82cc52")]
