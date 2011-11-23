897c3dad8f71d4b6aa2599e9e61b97895817ea15
Oliver, Jonathan <joliver@localhost>
2011-11-23 16:34:50 -0700

    Added tests and behavior to assert transactional/ack/rollback functionality of channel.

--- src/proj/NanoMessageBus.RabbitChannel/RabbitChannel.cs
+++ src/proj/NanoMessageBus.RabbitChannel/RabbitChannel.cs
@@ -5,30 +5,65 @@
 
 	public class RabbitChannel : IMessagingChannel
 	{
-		public virtual string ChannelGroup
-		{
-			get { return null; }
-		}
-		public virtual ChannelMessage CurrentMessage
-		{
-			get { return null; }
-		}
-		public virtual IChannelTransaction CurrentTransaction
-		{
-			get { return null; }
-		}
+		public virtual string ChannelGroup { get; private set; }
+		public virtual ChannelMessage CurrentMessage { get; private set; }
+		public virtual IChannelTransaction CurrentTransaction { get; private set; }
+
 		public virtual void Receive(Action<IDeliveryContext> callback)
 		{
+			if (callback == null)
+				throw new ArgumentNullException("callback");
+
+			// TODO: wrap exception if channel unavailable
+			this.subscription = this.subscriptionFactory(); // TODO: ensure this only gets called once
+			this.subscription.BeginReceive(DefaultTimeout, msg =>
+			{
+				this.CurrentMessage = null; // TODO: convert from RabbitMessage
+				this.CurrentTransaction = null; // TODO: start new transaction
+
+				callback(this);
+			});
 		}
 		public virtual void Send(ChannelEnvelope envelope)
 		{
 		}
 
-		public RabbitChannel(IModel channel, RabbitTransactionType transactionType)
+		public virtual void AcknowledgeMessage()
 		{
+			if (this.subscription == null)
+				throw new InvalidOperationException("The channel must first be opened for receive.");
+
+			if (this.transactionType == RabbitTransactionType.None)
+				return;
+
+			this.subscription.AcknowledgeReceipt(); // TODO: wrap exception if channel unavailable
+		}
+		public virtual void CommitTransaction()
+		{
+			if (this.transactionType == RabbitTransactionType.Full)
+				this.channel.TxCommit(); // TODO: wrap exception if channel unavailable
+		}
+		public virtual void RollbackTransaction()
+		{
+			if (this.transactionType == RabbitTransactionType.Full)
+				this.channel.TxRollback(); // TODO: wrap exception if channel unavailable
+		}
+
+		public RabbitChannel(
+			string channelGroup,
+			IModel channel,
+			RabbitTransactionType transactionType,
+			Func<RabbitSubscription> subscriptionFactory) : this()
+		{
+			this.ChannelGroup = channelGroup;
 			this.channel = channel;
 			this.transactionType = transactionType;
+			this.subscriptionFactory = subscriptionFactory;
+
+			if (transactionType == RabbitTransactionType.Full)
+				this.channel.TxSelect();
 		}
+		protected RabbitChannel() { }
 		~RabbitChannel()
 		{
 			this.Dispose(false);
@@ -43,7 +78,10 @@
 		{
 		}
 
+		private static readonly TimeSpan DefaultTimeout = TimeSpan.FromMilliseconds(500);
 		private readonly IModel channel;
 		private readonly RabbitTransactionType transactionType;
+		private readonly Func<RabbitSubscription> subscriptionFactory;
+		private RabbitSubscription subscription;
 	}
 }
--- src/tests/NanoMessageBus.RabbitChannel.UnitTests/RabbitChannelTests.cs
+++ src/tests/NanoMessageBus.RabbitChannel.UnitTests/RabbitChannelTests.cs
@@ -3,26 +3,237 @@
 
 namespace NanoMessageBus.RabbitChannel
 {
+	using System;
 	using Machine.Specifications;
 	using Moq;
 	using RabbitMQ.Client;
 	using It = Machine.Specifications.It;
 
 	[Subject(typeof(RabbitChannel))]
-	public class when_the_channel : using_a_channel
+	public class when_establishing_a_channel : using_a_channel
 	{
-		Establish context = () => { };
-		Because of = () => { };
-		It should_ = () => { };
+		It should_contain_the_name_provided = () =>
+			channel.ChannelGroup.ShouldEqual(DefaultChannelGroup);
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_opening_a_transactional_channel : using_a_channel
+	{
+		Establish context = () =>
+		{
+			mockRealChannel.Setup(x => x.TxSelect());
+			transactionType = RabbitTransactionType.Full;
+		};
+
+		Because of = () =>
+			Initialize();
+
+		It should_mark_the_underlying_channel_as_transactional = () =>
+			mockRealChannel.Verify(x => x.TxSelect(), Times.Once());
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_opening_the_channel_to_receive : using_a_channel
+	{
+		Establish context = () =>
+		{
+			channel = new RabbitChannel(DefaultChannelGroup, mockRealChannel.Object, transactionType, () =>
+			{
+				invocations++;
+				return mockSubscription.Object;
+			});
+		};
+
+		Because of = () =>
+			channel.Receive(delivery => { });
+
+		It should_call_the_subscription_factory = () =>
+			invocations.ShouldEqual(1);
+
+		It should_open_the_subscription_to_receive = () =>
+			mockSubscription.Verify(x => x.BeginReceive(Moq.It.IsAny<TimeSpan>(), Moq.It.IsAny<Action<RabbitMessage>>()));
+
+		static int invocations;
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_opening_the_channel_to_receive_without_provding_a_callback : using_a_channel
+	{
+		Because of = () =>
+			thrown = Catch.Exception(() => channel.Receive(null));
+
+		It should_throw_an_exception = () =>
+			thrown.ShouldBeOfType<ArgumentNullException>();
+
+		static Exception thrown;
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_acknowledging_a_message_against_an_acknowledge_only_channel : using_a_channel
+	{
+		Establish context = () =>
+		{
+			transactionType = RabbitTransactionType.Acknowledge;
+			mockSubscription.Setup(x => x.AcknowledgeReceipt());
+			Initialize();
+
+			channel.Receive(delivery => { });
+		};
+
+		Because of = () =>
+			channel.AcknowledgeMessage();
+
+		It should_ack_against_the_underlying_subscription = () =>
+			mockSubscription.Verify(x => x.AcknowledgeReceipt(), Times.Once());
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_acknowledging_a_message_with_no_transaction : using_a_channel
+	{
+		Establish context = () =>
+			channel.Receive(delivery => { });
+
+		Because of = () =>
+			channel.AcknowledgeMessage();
+
+		It should_NOT_ack_against_the_underlying_subscription = () =>
+			mockSubscription.Verify(x => x.AcknowledgeReceipt(), Times.Never());
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_acknowledging_a_message_on_a_full_transaction : using_a_channel
+	{
+		Establish context = () =>
+		{
+			transactionType = RabbitTransactionType.Full;
+			mockSubscription.Setup(x => x.AcknowledgeReceipt());
+			Initialize();
+
+			channel.Receive(delivery => { });
+		};
+
+		Because of = () =>
+			channel.AcknowledgeMessage();
+
+		It should_ack_against_the_underlying_subscription = () =>
+			mockSubscription.Verify(x => x.AcknowledgeReceipt(), Times.Once());
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_acknowledging_without_first_opening_for_receive : using_a_channel
+	{
+		Because of = () =>
+			thrown = Catch.Exception(() => channel.AcknowledgeMessage());
+
+		It should_throw_an_invalid_operation_exception = () =>
+			thrown.ShouldBeOfType<InvalidOperationException>();
+
+		It should_NOT_ack_against_the_underlying_subscription = () =>
+			mockSubscription.Verify(x => x.AcknowledgeReceipt(), Times.Never());
+
+		static Exception thrown;
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_committing_a_transaction_against_a_non_transactional_channel : using_a_channel
+	{
+		Establish context = () =>
+			mockRealChannel.Setup(x => x.TxCommit());
+
+		Because of = () =>
+			channel.CommitTransaction();
+
+		It should_NOT_invoke_commit_against_the_underlying_channel = () =>
+			mockRealChannel.Verify(x => x.TxCommit(), Times.Never());
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_committing_a_transaction_against_an_acknowledge_channel : using_a_channel
+	{
+		Establish context = () =>
+		{
+			transactionType = RabbitTransactionType.Acknowledge;
+			Initialize();
+
+			mockRealChannel.Setup(x => x.TxCommit());
+		};
+
+		Because of = () =>
+			channel.CommitTransaction();
+
+		It should_NOT_invoke_commit_against_the_underlying_channel = () =>
+			mockRealChannel.Verify(x => x.TxCommit(), Times.Never());
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_committing_a_transaction_against_a_transactional_channel : using_a_channel
+	{
+		Establish context = () =>
+		{
+			transactionType = RabbitTransactionType.Full;
+			Initialize();
+
+			mockRealChannel.Setup(x => x.TxCommit());
+		};
+
+		Because of = () =>
+			channel.CommitTransaction();
+
+		It should_NOT_invoke_commit_against_the_underlying_channel = () =>
+			mockRealChannel.Verify(x => x.TxCommit(), Times.Once());
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_rolling_back_a_transaction_against_a_non_transactional_channel : using_a_channel
+	{
+		Establish context = () =>
+			mockRealChannel.Setup(x => x.TxRollback());
+
+		Because of = () =>
+			channel.CommitTransaction();
+
+		It should_NOT_invoke_commit_against_the_underlying_channel = () =>
+			mockRealChannel.Verify(x => x.TxRollback(), Times.Never());
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_rolling_back_a_transaction_against_an_acknowledge_channel : using_a_channel
+	{
+		Establish context = () =>
+		{
+			transactionType = RabbitTransactionType.Acknowledge;
+			Initialize();
+
+			mockRealChannel.Setup(x => x.TxRollback());
+		};
+
+		Because of = () =>
+			channel.CommitTransaction();
+
+		It should_NOT_invoke_commit_against_the_underlying_channel = () =>
+			mockRealChannel.Verify(x => x.TxRollback(), Times.Never());
+	}
+
+	[Subject(typeof(RabbitChannel))]
+	public class when_rolling_back_a_transaction_against_a_transactional_channel : using_a_channel
+	{
+		Establish context = () =>
+		{
+			transactionType = RabbitTransactionType.Full;
+			Initialize();
+
+			mockRealChannel.Setup(x => x.TxRollback());
+		};
+
+		Because of = () =>
+			channel.CommitTransaction();
+
+		It should_NOT_invoke_commit_against_the_underlying_channel = () =>
+			mockRealChannel.Verify(x => x.TxRollback(), Times.Never());
 	}
 
 	public abstract class using_a_channel
 	{
-		protected static RabbitTransactionType transactionType = RabbitTransactionType.None;
-		protected static Mock<RabbitSubscription> mockSubscription;
-		protected static Mock<IModel> mockRealChannel;
-		protected static RabbitChannel channel;
-
 		Establish context = () =>
 		{
 			mockSubscription = new Mock<RabbitSubscription>();
@@ -33,7 +244,11 @@ namespace NanoMessageBus.RabbitChannel
 
 		protected static void Initialize()
 		{
-			channel = new RabbitChannel(mockRealChannel.Object, transactionType);
+			channel = new RabbitChannel(
+				DefaultChannelGroup,
+				mockRealChannel.Object,
+				transactionType,
+				() => mockSubscription.Object);
 		}
 
 		Cleanup after = () =>
@@ -42,6 +257,12 @@ namespace NanoMessageBus.RabbitChannel
 			mockRealChannel = null;
 			transactionType = RabbitTransactionType.None;
 		};
+
+		protected const string DefaultChannelGroup = "some group name";
+		protected static RabbitTransactionType transactionType = RabbitTransactionType.None;
+		protected static Mock<RabbitSubscription> mockSubscription;
+		protected static Mock<IModel> mockRealChannel;
+		protected static RabbitChannel channel;
 	}
 }
 
