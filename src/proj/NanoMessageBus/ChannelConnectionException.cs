64e2d62105bad712596ffaa9e56edf2f82035765
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-12-03 12:05:16 -0700

    Removed unused exception constructors.

--- src/proj/NanoMessageBus/ChannelConnectionException.cs
+++ src/proj/NanoMessageBus/ChannelConnectionException.cs
@@ -1,7 +1,6 @@
 ﻿namespace NanoMessageBus
 {
 	using System;
-	using System.Runtime.Serialization;
 
 	/// <summary>
 	/// Represents a persistent communcation failure during a channel message exchange.
@@ -20,28 +19,9 @@
 		/// Initializes a new instance of the ChannelConnectionException class.
 		/// </summary>
 		/// <param name="message">The message that describes the error.</param>
-		public ChannelConnectionException(string message)
-			: base(message)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the ChannelConnectionException class.
-		/// </summary>
-		/// <param name="message">The message that describes the error.</param>
 		/// <param name="inner">The exception that is the cause of the current exception.</param>
 		public ChannelConnectionException(string message, Exception inner)
 			: base(message, inner)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the ChannelConnectionException class.
-		/// </summary>
-		/// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
-		/// <param name="context">The StreamingContext that holds contextual information about the source or destination.</param>
-		protected ChannelConnectionException(SerializationInfo info, StreamingContext context)
-			: base(info, context)
 		{
 		}
 	}
--- src/proj/NanoMessageBus/DeadLetterException.cs
+++ src/proj/NanoMessageBus/DeadLetterException.cs
@@ -1,7 +1,6 @@
 ﻿namespace NanoMessageBus
 {
 	using System;
-	using System.Runtime.Serialization;
 
 	/// <summary>
 	/// Represents an exception that occurs when an expired message is received or a message is handled
@@ -10,40 +9,5 @@
 	[Serializable]
 	public class DeadLetterException : ChannelException
 	{
-		/// <summary>
-		/// Initializes a new instance of the DeadLetterException class.
-		/// </summary>
-		public DeadLetterException()
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the DeadLetterException class.
-		/// </summary>
-		/// <param name="message">The message that describes the error.</param>
-		public DeadLetterException(string message)
-			: base(message)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the DeadLetterException class.
-		/// </summary>
-		/// <param name="message">The message that describes the error.</param>
-		/// <param name="inner">The exception that is the cause of the current exception.</param>
-		public DeadLetterException(string message, Exception inner)
-			: base(message, inner)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the DeadLetterException class.
-		/// </summary>
-		/// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
-		/// <param name="context">The StreamingContext that holds contextual information about the source or destination.</param>
-		protected DeadLetterException(SerializationInfo info, StreamingContext context)
-			: base(info, context)
-		{
-		}
 	}
 }
