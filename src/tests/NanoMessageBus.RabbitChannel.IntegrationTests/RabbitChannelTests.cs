87c41ec1f7333facf68f3efa427f6e3131eb24c6
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-12-03 13:41:49 -0700

    Moved common test behavior into base class.

--- src/tests/NanoMessageBus.RabbitChannel.IntegrationTests/RabbitChannelTests.cs
+++ src/tests/NanoMessageBus.RabbitChannel.IntegrationTests/RabbitChannelTests.cs
@@ -4,7 +4,6 @@
 namespace NanoMessageBus.RabbitChannel
 {
 	using System;
-	using System.Collections.Generic;
 	using System.Configuration;
 	using System.Linq;
 	using System.Threading;
@@ -15,119 +14,160 @@ namespace NanoMessageBus.RabbitChannel
 	public class when_connecting_with_bad_credentials : using_the_channel
 	{
 		Establish context = () =>
-		{
-			factory.UserName = "bad user";
-			factory.Password = "bad password";
-		};
+			connectionFactory.Password = "bad password";
 
 		Because of = () =>
-			thrown = Catch.Exception(Connect);
+			thrown = Catch.Exception(() => OpenSender());
 
 		It should_throw_a_ChannelConnectionException = () =>
 			thrown.ShouldBeOfType<ChannelConnectionException>();
 	}
 
 	[Subject(typeof(RabbitChannel))]
+	public class when_dispatching_a_message : using_the_channel
+	{
+		Establish context = () =>
+			OpenSender();
+
+		Because of = () =>
+			thrown = Catch.Exception(() => senderChannel.Send(BuildEnvelope("some message")));
+
+		It should_not_throw_an_exception = () =>
+			thrown.ShouldBeNull();
+	}
+
+	[Subject(typeof(RabbitChannel))]
 	public class when_sending_and_then_receiving_a_message : using_the_channel
 	{
 		Establish context = () =>
-		{
-			configs.Add(new RabbitChannelGroupConfiguration()
-				.WithGroupName("receive")
-				.WithInputQueue("my-queue")
-				.WithMessageTypes(new[] { typeof(Guid) })
-				.WithCleanQueue());
-
-			Connect(); // for send
-
-			new Thread(() =>
-			{
-				var receiver = connector.Connect("receive");
-				receiver.Receive(delivery =>
-				{
-					received = (Guid)delivery.CurrentMessage.Messages.First();
-					receiver.BeginShutdown();
-				});
-			}).Start();
-		};
+			receiverConfig.WithCleanQueue();
 
 		Because of = () =>
-			channel.Send(BuildEnvelope(message, new Uri("direct://default/my-queue")));
+		{
+			OpenReceiver(delivery =>
+				(received = (string)delivery.CurrentMessage.Messages.First()).Length > 0);
+			OpenSender().Send(BuildEnvelope(message));
+		};
 
 		It should_wait_a_little_bit_to_receive_the_message = () =>
-		{
-			while (Guid.Empty == received)
-				Thread.Sleep(10);
-		};
+			WaitUntil(() => received != null, DefaultSleepTimeout);
 
 		It should_receive_the_message_that_was_sent = () =>
 			received.ShouldEqual(message);
 
-		static Guid received;
-		static readonly Guid message = Guid.NewGuid();
+		static string received;
+		static readonly string message = Guid.NewGuid().ToString();
 	}
 
 	[Subject(typeof(RabbitChannel))]
-	public class when_dispatching_a_message : using_the_channel
+	public class when_the_transaction_to_dispatch_a_message_is_not_committed
 	{
-		Establish context = () =>
-		{
-			var logicalMessages = new[] { "some message" };
-			var recipients = new[] { new Uri("fanout://system.string/") };
-			var message = new ChannelMessage(Guid.NewGuid(), Guid.NewGuid(), null, null, logicalMessages);
-			envelope = new ChannelEnvelope(message, recipients);
+		It should_not_dispatch_the_message;
+	}
 
-			config.WithMessageTypes(new[] { typeof(string) });
-			Connect();
-		};
-
-		Because of = () =>
-			thrown = Catch.Exception(() => channel.Send(envelope));
-
-		It should_not_throw_an_exception = () =>
-			thrown.ShouldBeNull();
-
-		static ChannelEnvelope envelope;
+	[Subject(typeof(RabbitChannel))]
+	public class when_the_transaction_to_receive_a_message_is_not_committed
+	{
+		It should_not_remove_the_message_from_the_input_queue;
 	}
 
 	public abstract class using_the_channel
 	{
 		Establish context = () =>
 		{
-			factory = new ConnectionFactory();
 			connector = null;
-			connectionUri = new Uri(ConfigurationManager.AppSettings["ConnectionUri"]);
-			config = new RabbitChannelGroupConfiguration();
-			configs = new LinkedList<RabbitChannelGroupConfiguration>(new[] { config });
+			senderChannel = null;
+			receiverChannel = null;
+
+			wireup = new RabbitWireup()
+				.WithConnectionFactory(connectionFactory = new ConnectionFactory())
+				.WithEndpoint(connectionUri = new Uri(ConfigurationManager.AppSettings["ConnectionUri"]))
+				.WithShutdownTimout(ShutdownTimeout)
+				.AddChannelGroup(x =>
+				{
+					senderConfig = x.WithGroupName(DefaultSenderChannelGroup);
+				})
+				.AddChannelGroup(x =>
+				{
+					receiverConfig = x
+						.WithGroupName(DefaultReceiverChannelGroup)
+						.WithInputQueue(DefaultReceiverInputQueue)
+						.WithMessageTypes(new[] { typeof(string) });
+				});
 		};
 
-		protected static void Connect()
+		protected static IMessagingChannel OpenSender()
 		{
-			connector = connector ?? new RabbitConnector(factory, ShutdownTimeout, configs);
-			factory.Endpoint = new AmqpTcpEndpoint(connectionUri);
-			channel = connector.Connect(config.GroupName);
+			return senderChannel = OpenConnection().Connect(senderConfig.GroupName);
 		}
+		protected static IMessagingChannel OpenReceiver()
+		{
+			return receiverChannel = OpenConnection().Connect(receiverConfig.GroupName);
+		}
+		protected static IChannelConnector OpenConnection()
+		{
+			return connector = connector ?? wireup.Build();
+		}
+
 		protected static ChannelEnvelope BuildEnvelope(object message, params Uri[] recipients)
 		{
+			if (recipients == null || recipients.Length == 0)
+				recipients = new[] { new Uri("direct://default/" + DefaultReceiverInputQueue) };
+
 			var channelMessage = new ChannelMessage(
 				Guid.NewGuid(), Guid.NewGuid(), null, null, new[] { message });
 			return new ChannelEnvelope(channelMessage, recipients);
 		}
 
+		protected static void OpenReceiver(Func<IDeliveryContext, bool> callback)
+		{
+			OpenReceiver();
+			new Thread(() => receiverChannel.Receive(delivery =>
+			{
+				if (!callback(delivery))
+					receiverChannel.BeginShutdown();
+			})).Start();
+		}
+		protected static void WaitUntil(Func<bool> callback, TimeSpan maxWait)
+		{
+			var started = SystemTime.UtcNow;
+
+			while (!callback())
+			{
+				Thread.Sleep(10);
+				if ((SystemTime.UtcNow - started) > maxWait)
+					break;
+			}
+		}
+
 		Cleanup after = () =>
 		{
-			channel.BeginShutdown();
-			channel.Dispose();
-			connector.Dispose();
+			if (senderChannel != null)
+				senderChannel.Dispose();
+
+			if (receiverChannel != null)
+			{
+				receiverChannel.BeginShutdown();
+				receiverChannel.Dispose();	
+			}
+
+			if (connector != null)
+				connector.Dispose();
 		};
 
+		protected const string DefaultReceiverChannelGroup = "receiver";
+		protected const string DefaultSenderChannelGroup = "sender";
+		protected const string DefaultReceiverInputQueue = "my-queue";
+		protected static readonly TimeSpan DefaultSleepTimeout = TimeSpan.FromMilliseconds(250);
 		static readonly TimeSpan ShutdownTimeout = TimeSpan.FromMilliseconds(100);
-		protected static ConnectionFactory factory;
+		protected static RabbitWireup wireup;
+		protected static ConnectionFactory connectionFactory;
 		protected static Uri connectionUri;
-		protected static RabbitChannelGroupConfiguration config;
-		protected static ICollection<RabbitChannelGroupConfiguration> configs;
+		protected static RabbitChannelGroupConfiguration receiverConfig;
+		protected static RabbitChannelGroupConfiguration senderConfig;
 		protected static RabbitConnector connector;
-		protected static IMessagingChannel channel;
+		protected static IMessagingChannel senderChannel;
+		protected static IMessagingChannel receiverChannel;
 		protected static Exception thrown;
 	}
 }
