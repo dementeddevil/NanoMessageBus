5188dd9c43c1c6903af3adc1ab3835f1587e20ed
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-12-05 06:16:55 -0700

    Added tests around SystemTime.

---
+++ src/tests/NanoMessageBus.UnitTests/SystemTimeTests.cs
@@ -0,0 +1,66 @@
+﻿#pragma warning disable 169
+// ReSharper disable InconsistentNaming
+
+namespace NanoMessageBus
+{
+	using System;
+	using System.Threading;
+	using Machine.Specifications;
+
+	[Subject(typeof(SystemTime))]
+	public class when_resolving_the_system_time
+	{
+		It should_be_the_current_time = () =>
+			SystemTime.UtcNow.ShouldBeCloseTo(DateTime.UtcNow, TimeSpan.FromMilliseconds(10));
+	}
+
+	[Subject(typeof(SystemTime))]
+	public class when_setting_the_system_time
+	{
+		Because of = () =>
+			SystemTime.Resolver = () => Instant;
+
+		It should_be_the_time_specified = () =>
+			SystemTime.UtcNow.ShouldEqual(Instant);
+
+		It should_remain_the_time_specified = () =>
+		{
+			Thread.Sleep(1);
+			SystemTime.UtcNow.ShouldEqual(Instant);
+		};
+
+		Cleanup after = () =>
+			SystemTime.Resolver = null;
+
+		static readonly DateTime Instant = DateTime.Parse("2010-01-02 03:04:05.678");
+	}
+
+	[Subject(typeof(SystemTime))]
+	public class when_getting_epoch_time
+	{
+		Establish context = () =>
+			SystemTime.Resolver = () => Instant;
+
+		Because of = () =>
+		{
+			seconds = SystemTime.UtcNow.ToEpochTime();
+			converted = seconds.ToDateTime();
+		};
+
+		It should_yield_the_number_of_seconds_since_Unix_epoch_time_1970 = () =>
+			seconds.ShouldEqual(946684800);
+
+		It should_be_able_to_convert_epoch_time_back_to_a_regular_DateTime = () =>
+			converted.ShouldEqual(Instant);
+
+		Cleanup after = () =>
+			SystemTime.Resolver = null;
+
+		static long seconds;
+		static DateTime converted;
+		static readonly DateTime Instant = new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+	}
+}
+
+// ReSharper enable InconsistentNaming
+#pragma warning restore 169
