227fa4a79bd4121db8233eeb6a5b829dcb5286b4
Oliver, Jonathan <joliver@localhost>
2012-01-13 16:03:01 -0700

    Stubbed out empty classes that will facilitate scoping of dependency resolution in the user code.

---
+++ src/proj/NanoMessageBus/DependencyResolverChannel.cs
@@ -0,0 +1,53 @@
+﻿namespace NanoMessageBus
+{
+	using System;
+
+	public class DependencyResolverChannel : IMessagingChannel
+	{
+		public ChannelMessage CurrentMessage
+		{
+			get { return null; }
+		}
+		public IChannelTransaction CurrentTransaction
+		{
+			get { return null; }
+		}
+		public IDependencyResolver CurrentResolver
+		{
+			get { return null; }
+		}
+
+		public void Send(ChannelEnvelope envelope)
+		{
+		}
+		public void BeginShutdown()
+		{
+		}
+		public void Receive(Action<IDeliveryContext> callback)
+		{
+		}
+
+		public DependencyResolverChannel(IMessagingChannel channel, IDependencyResolver resolver)
+		{
+			this.channel = channel;
+			this.resolver = resolver;
+		}
+		~DependencyResolverChannel()
+		{
+			this.Dispose(false);
+		}
+
+		public void Dispose()
+		{
+			this.Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		protected virtual void Dispose(bool disposing)
+		{
+		}
+
+		private readonly IMessagingChannel channel;
+		private readonly IDependencyResolver resolver;
+		private IDependencyResolver current;
+	}
+}
---
+++ src/proj/NanoMessageBus/DependencyResolverConnector.cs
@@ -0,0 +1,41 @@
+﻿namespace NanoMessageBus
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class DependencyResolverConnector : IChannelConnector
+	{
+		public ConnectionState CurrentState
+		{
+			get { return 0; }
+		}
+		public IEnumerable<IChannelGroupConfiguration> ChannelGroups
+		{
+			get { return null; }
+		}
+		public IMessagingChannel Connect(string channelGroup)
+		{
+			return null;
+		}
+
+		public DependencyResolverConnector(IChannelConnector connector)
+		{
+			this.connector = connector;
+		}
+		~DependencyResolverConnector()
+		{
+			this.Dispose(false);
+		}
+
+		public void Dispose()
+		{
+			this.Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		protected virtual void Dispose(bool disposing)
+		{
+		}
+
+		private readonly IChannelConnector connector;
+	}
+}
