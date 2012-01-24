7b5d0cd316ffedd6c0a3ac4e52b1f8d50ed63a43
Oliver, Jonathan <joliver@localhost>
2012-01-23 17:05:01 -0700

    Added spike/untested wireup code.

---
+++ src/proj/NanoMessageBus/DefaultMessagingHostWireup.cs
@@ -0,0 +1,50 @@
+﻿namespace NanoMessageBus
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public class DefaultMessagingHostWireup
+	{
+		public virtual DefaultMessagingHostWireup AddConnector(Func<IChannelConnector> callback)
+		{
+			this.connectors.Add(new DependencyResolverConnector(callback()));
+			return this;
+		}
+		public virtual DefaultMessagingHostWireup WithHandlerContext(Action<IDeliveryContext> delivery)
+		{
+			this.receive = delivery;
+			return this;
+		}
+		public virtual DefaultMessagingHostWireup WithRoutingTable(IRoutingTable table)
+		{
+			this.routingTable = table;
+			return this;
+		}
+		public virtual DefaultMessagingHostWireup WithDispatchTable(IDispatchTable table)
+		{
+			this.dispatchTable = table;
+			return this;
+		}
+		protected virtual void DefaultReceive(IDeliveryContext delivery)
+		{
+			using (var context = new DefaultHandlerContext(delivery, this.dispatchTable))
+				this.routingTable.Route(context, delivery.CurrentMessage);
+
+			delivery.CurrentTransaction.Commit();
+		}
+
+		public virtual IMessagingHost Build()
+		{
+			var host = new DefaultMessagingHost(this.connectors.ToArray(), new DefaultChannelGroupFactory().Build);
+			host.Initialize();
+			host.BeginReceive(this.receive ?? this.DefaultReceive);
+			return host;
+		}
+
+		private readonly ICollection<IChannelConnector> connectors = new LinkedList<IChannelConnector>();
+		private Action<IDeliveryContext> receive;
+		private IRoutingTable routingTable;
+		private IDispatchTable dispatchTable;
+	}
+}
