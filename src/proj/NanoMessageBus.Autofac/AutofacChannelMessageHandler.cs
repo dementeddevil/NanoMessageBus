dcc0d27042d0b2bc92ac183d439e887fe5b426bc
Oliver, Jonathan <joliver@localhost>
2012-02-03 12:48:53 -0700

    Added ChannelMessage handler to make IHandlerContext available to inject.

---
+++ src/proj/NanoMessageBus.Autofac/AutofacChannelMessageHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace NanoMessageBus
+{
+	using Autofac;
+
+	public class AutofacChannelMessageHandler : IMessageHandler<ChannelMessage>
+	{
+		public void Handle(ChannelMessage message)
+		{
+			this.handler.Handle(message);
+		}
+
+		public AutofacChannelMessageHandler(IHandlerContext context, IMessageHandler<ChannelMessage> inner = null)
+		{
+			var container = context.CurrentResolver.As<ILifetimeScope>();
+
+			var builder = new ContainerBuilder();
+			builder.RegisterInstance(context).ExternallyOwned(); // single instance for this and decendent scopes
+			builder.Update(container.ComponentRegistry);
+
+			this.handler = inner ?? new DefaultChannelMessageHandler(context, container.Resolve<IRoutingTable>());
+		}
+
+		private readonly IMessageHandler<ChannelMessage> handler;
+	}
+}
