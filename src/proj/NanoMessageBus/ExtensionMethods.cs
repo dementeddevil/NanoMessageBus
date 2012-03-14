13677e32ec6b5d8e14caca8e619928a0df17d01c
Oliver, Jonathan <joliver@localhost>
2012-03-13 21:17:13 -0600

    Added helper method to dictionary.

--- src/proj/NanoMessageBus/ExtensionMethods.cs
+++ src/proj/NanoMessageBus/ExtensionMethods.cs
@@ -1,6 +1,7 @@
 ﻿namespace NanoMessageBus
 {
 	using System;
+	using System.Collections.Generic;
 	using System.Globalization;
 
 	public static class ExtensionMethods
@@ -13,6 +14,12 @@
 		public static string ToNull(this Guid value)
 		{
 			return value == Guid.Empty ? null : value.ToString();
+		}
+
+		public static TValue TryGetValue<TKey, TValue>(this IDictionary<TKey, TValue> values, TKey key)
+		{
+			TValue value;
+			return values.TryGetValue(key, out value) ? value : default(TValue);
 		}
 	}
 }
