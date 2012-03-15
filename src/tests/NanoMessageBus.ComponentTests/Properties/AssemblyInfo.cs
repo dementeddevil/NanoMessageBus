8fd0a5c5f06be1f6fbf225af9e15f2d607d9b1d1
Oliver, Jonathan <joliver@localhost>
2012-03-15 10:13:34 -0600

    Removed unused projects.

--- src/tests/NanoMessageBus.ComponentTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NanoMessageBus.ComponentTests")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("4b858332-4268-47ff-b924-021b9e0d5c71")]
--- src/tests/NanoMessageBus.RabbitChannel.ComponentTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NanoMessageBus.RabbitChannel.ComponentTests")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("ba26e670-ad4d-4096-b5fd-fc7eb188fefa")]
