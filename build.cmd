f967c9c9e5b9c428868acb457c7df9242f00bd24
Oliver, Jonathan <joliver@localhost>
2012-03-16 10:59:42 -0600

    Added ILMerge to build process.

--- build.cmd
+++ build.cmd
@@ -1,9 +1,20 @@
 @echo off
-set path=%path%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;
+set path=%path%;C:/Windows/Microsoft.NET/Framework/v4.0.30319;
 
-msbuild src\NanoMessageBus.sln /nologo /v:q /p:Configuration=Release /t:Clean
-msbuild src\NanoMessageBus.sln /nologo /v:q /p:Configuration=Release
+echo Building project...
+msbuild src/NanoMessageBus.sln /nologo /v:q /p:Configuration=Release /t:Clean
+msbuild src/NanoMessageBus.sln /nologo /v:q /p:Configuration=Release /clp:ErrorsOnly
 
-::ILMerge JSON.NET
+echo Merging assemblies...
+del src\proj\NanoMessageBus.JsonSerializer\bin\Release\NanoMessageBus.*
+del src\proj\NanoMessageBus.JsonSerializer\bin\Release\*.xml
+if exist "src\proj\NanoMessageBus.JsonSerializer\bin\Release\merged" rmdir /s /q "src\proj\NanoMessageBus.JsonSerializer\bin\Release\merged"
+mkdir src\proj\NanoMessageBus.JsonSerializer\bin\Release\merged
+bin\ilmerge\ILMerge.exe /keyfile:src\NanoMessageBus.snk /internalize /wildcards /target:library ^
+ /targetplatform:"v4,C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319" ^
+ /out:"src\proj\NanoMessageBus.JsonSerializer\bin\Release\merged\NanoMessageBus.JsonSerializer.dll" ^
+ "src/proj/NanoMessageBus.JsonSerializer/bin/Release/*.dll"
+ 
+echo Creating NuGet packages...
 
-::Create NuGet Packages
+echo Done.
