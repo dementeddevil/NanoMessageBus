f3987d6114a6ceb9b501cd6e1be8280e652b75de
Oliver, Jonathan <joliver@localhost>
2012-03-16 09:39:38 -0600

    Added empty projects for Log4Net and NLog.

---
+++ src/proj/NanoMessageBus.Log4Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NanoMessageBus.Log4Net")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("0f661aeb-3b13-446d-ae1f-4343259547bd")]
---
+++ src/proj/NanoMessageBus.NLog/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NanoMessageBus.NLog")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("c89ecf79-0dcd-4599-83de-bc36d1c7b199")]
