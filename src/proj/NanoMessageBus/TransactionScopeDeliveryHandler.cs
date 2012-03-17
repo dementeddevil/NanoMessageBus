fc5519dbfccdeb5fc472d36228c0738a22bea083
Oliver, Jonathan <joliver@localhost>
2012-03-17 07:24:39 -0600

    Added logging to delivery handler.

--- src/proj/NanoMessageBus/TransactionScopeDeliveryHandler.cs
+++ src/proj/NanoMessageBus/TransactionScopeDeliveryHandler.cs
@@ -2,14 +2,18 @@
 {
 	using System;
 	using System.Transactions;
+	using Logging;
 
 	public class TransactionScopeDeliveryHandler : IDeliveryHandler
 	{
 		public virtual void Handle(IDeliveryContext delivery)
 		{
+			Log.Debug("Creating new transaction scope associated for delivery.");
 			using (var scope = new TransactionScope(this.scopeOption, this.transactionOptions))
 			{
 				this.inner.Handle(delivery);
+
+				Log.Debug("Committing transaction scope associated with delivery.");
 				scope.Complete();
 			}
 		}
@@ -23,9 +27,7 @@
 		{
 		}
 		public TransactionScopeDeliveryHandler(
-			IDeliveryHandler inner,
-			TransactionScopeOption scopeOption,
-			TransactionOptions transactionOptions)
+			IDeliveryHandler inner, TransactionScopeOption scopeOption, TransactionOptions transactionOptions)
 		{
 			if (inner == null)
 				throw new ArgumentNullException("inner");
@@ -38,5 +40,6 @@
 		private readonly IDeliveryHandler inner;
 		private readonly TransactionScopeOption scopeOption;
 		private readonly TransactionOptions transactionOptions;
+		private static readonly ILog Log = LogFactory.Build(typeof(TransactionScopeDeliveryHandler));
 	}
 }
