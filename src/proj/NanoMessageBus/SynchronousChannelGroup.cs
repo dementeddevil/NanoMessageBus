6ab0cb9222f0f3feac31a7cb4e4ba29389800091
Oliver, Jonathan <joliver@localhost>
2012-03-19 06:29:35 -0600

    Added a SynchronousChannelGroup to be used for synchronous configurations.

---
+++ src/proj/NanoMessageBus/SynchronousChannelGroup.cs
@@ -0,0 +1,91 @@
+﻿namespace NanoMessageBus
+{
+	using System;
+	using Logging;
+
+	public class SynchronousChannelGroup : IChannelGroup
+	{
+		public bool DispatchOnly
+		{
+			get { return this.configuration.DispatchOnly; }
+		}
+
+		public void Initialize()
+		{
+			this.ThrowWhenDisposed();
+
+			Log.Info("Initializing channel group '{0}'.", this.configuration.GroupName);
+			this.initialized = true;
+		}
+		public IMessagingChannel OpenChannel()
+		{
+			this.ThrowWhenDisposed();
+			this.ThrowWhenUninitialized();
+
+			Log.Debug("Opening a caller-owned channel for group '{0}'.", this.configuration.GroupName);
+			return this.connector.Connect(this.configuration.GroupName);
+		}
+
+		public void BeginReceive(Action<IDeliveryContext> callback)
+		{
+			throw new NotSupportedException();
+		}
+		public bool BeginDispatch(Action<IDispatchContext> callback)
+		{
+			throw new NotSupportedException();
+		}
+
+		protected virtual void ThrowWhenDisposed()
+		{
+			if (!this.disposed)
+				return;
+
+			Log.Warn("The channel group has been disposed.");
+			throw new ObjectDisposedException(typeof(DefaultChannelGroup).Name);
+		}
+		protected virtual void ThrowWhenUninitialized()
+		{
+			if (this.initialized)
+				return;
+
+			Log.Warn("The channel group has not been initialized.");
+			throw new InvalidOperationException("The channel group has not been initialized.");
+		}
+
+		public SynchronousChannelGroup(IChannelConnector connector, IChannelGroupConfiguration configuration)
+		{
+			if (connector == null)
+				throw new ArgumentNullException("connector");
+
+			if (configuration == null)
+				throw new ArgumentNullException("configuration");
+
+			this.connector = connector;
+			this.configuration = configuration;
+		}
+		~SynchronousChannelGroup()
+		{
+			this.Dispose(false);
+		}
+
+		public void Dispose()
+		{
+			this.Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposing || this.disposed)
+				return;
+
+			Log.Info("Channel group disposed.");
+			this.disposed = true;
+		}
+
+		private static readonly ILog Log = LogFactory.Build(typeof(SynchronousChannelGroup));
+		private readonly IChannelConnector connector;
+		private readonly IChannelGroupConfiguration configuration;
+		private bool initialized;
+		private bool disposed;
+	}
+}
---
+++ src/tests/NanoMessageBus.UnitTests/SynchronousChannelGroupTests.cs
@@ -0,0 +1,187 @@
+﻿#pragma warning disable 169
+// ReSharper disable InconsistentNaming
+
+namespace NanoMessageBus
+{
+	using System;
+	using System.Configuration;
+	using Machine.Specifications;
+	using Moq;
+	using It = Machine.Specifications.It;
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_a_null_connector_is_provided_during_constructing : using_a_synchronous_channel_group
+	{
+		Because of = () =>
+			Try(() => new SynchronousChannelGroup(null, mockConfiguration.Object));
+
+		It should_throw_an_exception = () =>
+			thrown.ShouldBeOfType<ArgumentNullException>();
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_a_null_configuration_is_provided_during_constructing : using_a_synchronous_channel_group
+	{
+		Because of = () =>
+			Try(() => new SynchronousChannelGroup(mockConnector.Object, null));
+
+		It should_throw_an_exception = () =>
+			thrown.ShouldBeOfType<ArgumentNullException>();
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_the_channel_group_has_been_constructed : using_a_synchronous_channel_group
+	{
+		Establish context = () =>
+			mockConfiguration.Setup(x => x.DispatchOnly).Returns(true);
+
+		It should_expose_the_dispatch_only_status_from_the_underlying_configuration = () =>
+			channelGroup.DispatchOnly.ShouldEqual(mockConfiguration.Object.DispatchOnly);
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_initialize_is_called : using_a_synchronous_channel_group
+	{
+		Because of = () =>
+			channelGroup.Initialize();
+
+		It should_not_do_anything = () =>
+			thrown.ShouldBeNull();
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_initialize_is_called_multiple_times : using_a_synchronous_channel_group
+	{
+		Because of = () =>
+			channelGroup.Initialize();
+
+		It should_not_throw_an_exception = () =>
+			thrown.ShouldBeNull();
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_initialize_is_called_after_the_group_has_been_disposed : using_a_synchronous_channel_group
+	{
+		Establish context = () =>
+			channelGroup.Dispose();
+
+		Because of = () =>
+			Try(() => channelGroup.Initialize());
+
+		It should_throw_an_exception = () =>
+			thrown.ShouldBeOfType<ObjectDisposedException>();
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_attempting_to_asynchronously_receive : using_a_synchronous_channel_group
+	{
+		Because of = () =>
+			Try(() => channelGroup.BeginReceive(null));
+
+		It should_throw_an_exception = () =>
+			thrown.ShouldBeOfType<NotSupportedException>();
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_attempting_to_asynchronously_dispatch : using_a_synchronous_channel_group
+	{
+		Because of = () =>
+			Try(() => channelGroup.BeginDispatch(null));
+
+		It should_throw_an_exception = () =>
+			thrown.ShouldBeOfType<NotSupportedException>();
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_opening_a_channel_on_an_uninitialized_group : using_a_synchronous_channel_group
+	{
+		Because of = () =>
+			Try(() => channelGroup.OpenChannel());
+
+		It should_throw_an_exception = () =>
+			thrown.ShouldBeOfType<InvalidOperationException>();
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_opening_a_channel_on_a_disposed_group : using_a_synchronous_channel_group
+	{
+		Establish context = () =>
+			channelGroup.Dispose();
+
+		Because of = () =>
+			Try(() => channelGroup.OpenChannel());
+
+		It should_throw_an_exception = () =>
+			thrown.ShouldBeOfType<ObjectDisposedException>();
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_opening_a_channel : using_a_synchronous_channel_group
+	{
+		Establish context = () =>
+		{
+			mockChannel = new Mock<IMessagingChannel>();
+			mockConnector.Setup(x => x.Connect(ChannelGroupName)).Returns(mockChannel.Object);
+
+			channelGroup.Initialize();
+		};
+
+		Because of = () =>
+			opened = channelGroup.OpenChannel();
+
+		It should_attempt_to_open_a_channel_on_the_underlying_connector = () =>
+			mockConnector.Verify(x => x.Connect(ChannelGroupName));
+
+		It should_return_a_reference_to_the_opened_channel = () =>
+			opened.ShouldEqual(mockChannel.Object);
+
+		static Mock<IMessagingChannel> mockChannel;
+		static IMessagingChannel opened;
+	}
+
+	[Subject(typeof(SynchronousChannelGroup))]
+	public class when_the_channel_connector_throws_an_exception : using_a_synchronous_channel_group
+	{
+		Establish context = () =>
+		{
+			mockConnector.Setup(x => x.Connect(ChannelGroupName)).Throws(new ConfigurationErrorsException());
+			channelGroup.Initialize();
+		};
+
+		Because of = () =>
+			Try(() => channelGroup.OpenChannel());
+
+		It should_not_catch_the_exception = () =>
+			thrown.ShouldBeOfType<ConfigurationErrorsException>();
+	}
+
+	public abstract class using_a_synchronous_channel_group
+	{
+		Establish context = () =>
+		{
+			mockConfiguration = new Mock<IChannelGroupConfiguration>();
+			mockConfiguration.Setup(x => x.GroupName).Returns(ChannelGroupName);
+
+			mockConnector = new Mock<IChannelConnector>();
+			mockConnector.Setup(x => x.ChannelGroups).Returns(new[] { mockConfiguration.Object });
+
+			thrown = null;
+
+			channelGroup = new SynchronousChannelGroup(mockConnector.Object, mockConfiguration.Object);
+		};
+
+		protected static void Try(Action callback)
+		{
+			thrown = Catch.Exception(callback);
+		}
+
+		protected const string ChannelGroupName = "Sync Group";
+		protected static SynchronousChannelGroup channelGroup;
+		protected static Mock<IChannelGroupConfiguration> mockConfiguration;
+		protected static Mock<IChannelConnector> mockConnector;
+		protected static Exception thrown;
+	}
+}
+
+// ReSharper enable InconsistentNaming
+#pragma warning restore 169
