75a1d57b03939c6f7ece7a0b9216890f803167f0
Oliver, Jonathan <joliver@localhost>
2012-03-21 14:04:27 -0600

    Bumped version number.

--- src/proj/VersionAssemblyInfo.cs
+++ src/proj/VersionAssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Reflection;
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: AssemblyVersion("2.0.5.0")]
-[assembly: AssemblyFileVersion("2.0.5.0")]
-//// [assembly: AssemblyInformationalVersion("2.0.5.0 Release")]
+[assembly: AssemblyVersion("2.0.6.0")]
+[assembly: AssemblyFileVersion("2.0.6.0")]
+//// [assembly: AssemblyInformationalVersion("2.0.6.0 Release")]
