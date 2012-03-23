9734935e20198e73c2cabe8ca56035d95b589548
Oliver, Jonathan <joliver@localhost>
2012-03-23 16:32:07 -0600

    Creating additional logging package for multiple version of Log4Net.

---
+++ src/proj/NanoMessageBus.Log4Net2/Log4NetLogger.cs
@@ -0,0 +1,48 @@
+namespace NanoMessageBus.Logging
+{
+	using System;
+
+	public class Log4NetLogger : ILog
+	{
+		private readonly log4net.ILog log;
+
+		public Log4NetLogger(Type typeToLog, bool autoConfigure = true)
+		{
+			if (autoConfigure)
+				log4net.Config.XmlConfigurator.Configure();
+
+			this.log = log4net.LogManager.GetLogger(typeToLog);
+		}
+
+		public virtual void Verbose(string message, params object[] values)
+		{
+			if (this.log.IsDebugEnabled)
+				this.log.DebugFormat(message, values);
+		}
+		public virtual void Debug(string message, params object[] values)
+		{
+			if (this.log.IsDebugEnabled)
+				this.log.DebugFormat(message, values);
+		}
+		public virtual void Info(string message, params object[] values)
+		{
+			if (this.log.IsInfoEnabled)
+				this.log.InfoFormat(message, values);
+		}
+		public virtual void Warn(string message, params object[] values)
+		{
+			if (this.log.IsWarnEnabled)
+				this.log.WarnFormat(message, values);
+		}
+		public virtual void Error(string message, params object[] values)
+		{
+			if (this.log.IsErrorEnabled)
+				this.log.ErrorFormat(message, values);
+		}
+		public virtual void Fatal(string message, params object[] values)
+		{
+			if (this.log.IsFatalEnabled)
+				this.log.FatalFormat(message, values);
+		}
+	}
+}
---
+++ src/proj/NanoMessageBus.Log4Net2/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NanoMessageBus.Log4Net2")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("23387677-3ba7-4ae9-95c7-a160bb816549")]
