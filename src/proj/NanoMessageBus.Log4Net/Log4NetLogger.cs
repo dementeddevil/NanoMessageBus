31eb7537e09dc2140036be658812dca522ceef7c
Oliver, Jonathan <joliver@localhost>
2012-03-27 10:38:08 -0600

    Simplified loggers to improve performance.

--- src/proj/NanoMessageBus.Log4Net/Log4NetLogger.cs
+++ src/proj/NanoMessageBus.Log4Net/Log4NetLogger.cs
@@ -4,7 +4,31 @@ namespace NanoMessageBus.Logging
 
 	public class Log4NetLogger : ILog
 	{
-		private readonly log4net.ILog log;
+		public virtual void Verbose(string message, params object[] values)
+		{
+			// no op
+		}
+		public virtual void Debug(string message, params object[] values)
+		{
+			if (this.debug)
+				this.log.DebugFormat(message, values);
+		}
+		public virtual void Info(string message, params object[] values)
+		{
+			this.log.InfoFormat(message, values);
+		}
+		public virtual void Warn(string message, params object[] values)
+		{
+			this.log.WarnFormat(message, values);
+		}
+		public virtual void Error(string message, params object[] values)
+		{
+			this.log.ErrorFormat(message, values);
+		}
+		public virtual void Fatal(string message, params object[] values)
+		{
+			this.log.FatalFormat(message, values);
+		}
 
 		public Log4NetLogger(Type typeToLog, bool autoConfigure = true)
 		{
@@ -12,36 +36,10 @@ namespace NanoMessageBus.Logging
 				log4net.Config.XmlConfigurator.Configure();
 
 			this.log = log4net.LogManager.GetLogger(typeToLog);
+			this.debug = this.log.IsDebugEnabled;
 		}
 
-		public virtual void Verbose(string message, params object[] values)
-		{
-			// no op
-		}
-		public virtual void Debug(string message, params object[] values)
-		{
-			if (this.log.IsDebugEnabled)
-				this.log.DebugFormat(message, values);
-		}
-		public virtual void Info(string message, params object[] values)
-		{
-			if (this.log.IsInfoEnabled)
-				this.log.InfoFormat(message, values);
-		}
-		public virtual void Warn(string message, params object[] values)
-		{
-			if (this.log.IsWarnEnabled)
-				this.log.WarnFormat(message, values);
-		}
-		public virtual void Error(string message, params object[] values)
-		{
-			if (this.log.IsErrorEnabled)
-				this.log.ErrorFormat(message, values);
-		}
-		public virtual void Fatal(string message, params object[] values)
-		{
-			if (this.log.IsFatalEnabled)
-				this.log.FatalFormat(message, values);
-		}
+		private readonly log4net.ILog log;
+		private readonly bool debug;
 	}
 }
--- src/proj/NanoMessageBus.NLog/NLogLogger.cs
+++ src/proj/NanoMessageBus.NLog/NLogLogger.cs
@@ -6,47 +6,49 @@ namespace NanoMessageBus.Logging
 
 	public class NLogLogger : ILog
 	{
-		private static readonly NLog.LogFactory Factory = new NLog.LogFactory();
-		private readonly Logger log;
-
-		public NLogLogger(Type typeToLog)
-		{
-			this.log = Factory.GetLogger(typeToLog.FullName);
-		}
-		public NLogLogger(Type typeToLog, LoggingConfiguration configuration)
-		{
-			this.log = new NLog.LogFactory(configuration).GetLogger(typeToLog.FullName);
-		}
-
 		public virtual void Verbose(string message, params object[] values)
 		{
-			if (this.log.IsTraceEnabled)
+			if (this.trace)
 				this.log.Trace(message, values);
 		}
 		public virtual void Debug(string message, params object[] values)
 		{
-			if (this.log.IsDebugEnabled)
+			if (this.debug)
 				this.log.Debug(message, values);
 		}
 		public virtual void Info(string message, params object[] values)
 		{
-			if (this.log.IsInfoEnabled)
-				this.log.Info(message, values);
+			this.log.Info(message, values);
 		}
 		public virtual void Warn(string message, params object[] values)
 		{
-			if (this.log.IsWarnEnabled)
-				this.log.Warn(message, values);
+			this.log.Warn(message, values);
 		}
 		public virtual void Error(string message, params object[] values)
 		{
-			if (this.log.IsErrorEnabled)
-				this.log.Error(message, values);
+			this.log.Error(message, values);
 		}
 		public virtual void Fatal(string message, params object[] values)
 		{
-			if (this.log.IsFatalEnabled)
-				this.log.Fatal(message, values);
+			this.log.Fatal(message, values);
 		}
+
+		public NLogLogger(Type typeToLog)
+		{
+			this.log = Factory.GetLogger(typeToLog.FullName);
+			this.trace = this.log.IsTraceEnabled;
+			this.debug = this.log.IsDebugEnabled;
+		}
+		public NLogLogger(Type typeToLog, LoggingConfiguration configuration)
+		{
+			this.log = new NLog.LogFactory(configuration).GetLogger(typeToLog.FullName);
+			this.trace = this.log.IsTraceEnabled;
+			this.debug = this.log.IsDebugEnabled;
+		}
+
+		private static readonly NLog.LogFactory Factory = new NLog.LogFactory();
+		private readonly Logger log;
+		private readonly bool debug;
+		private readonly bool trace;
 	}
 }
