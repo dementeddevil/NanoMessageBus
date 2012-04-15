fef35eb000f7da2f81bcacb4bbad4d3fdee2c595
Oliver, Jonathan <joliver@localhost>
2012-04-14 21:15:10 -0600

    Pointing to latest Json.NET serializer and mspec test binaries.

--- src/proj/NanoMessageBus/IDeliveryHandler.cs
+++ src/proj/NanoMessageBus/IDeliveryHandler.cs
@@ -3,6 +3,9 @@
 	/// <summary>
 	/// Provides the ability to wrap the delivery of a message.
 	/// </summary>
+	/// <remarks>
+	/// Instances of this class must be designed to be multi-thread safe such that they can be shared between threads.
+	/// </remarks>
 	public interface IDeliveryHandler : IMessageHandler<IDeliveryContext>
 	{
 	}
