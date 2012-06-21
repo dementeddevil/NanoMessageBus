d3f9afe6194211e38d45ee862b3fff2268ca16c3
Oliver, Jonathan <joliver@localhost>
2012-06-21 16:17:01 -0600

    Issue #49 cloning of HttpContext now possible.

---
+++ src/proj/NanoMessageBus/Channels/HttpRequestAuditorExtensions.cs
@@ -0,0 +1,281 @@
+﻿namespace NanoMessageBus.Channels
+{
+	using System;
+	using System.Collections.Specialized;
+	using System.Security.Principal;
+	using System.Text;
+	using System.Web;
+
+	public static class HttpRequestAuditorExtensions
+	{
+		public static HttpContextBase Clone(this HttpContext context)
+		{
+			return (new HttpContextWrapper(context)).Clone();
+		}
+		public static HttpContextBase Clone(this HttpContextBase context)
+		{
+			return new HttpContextClone(context);
+		}
+	}
+
+	internal class HttpContextClone : HttpContextBase
+	{
+		public override Exception Error
+		{
+			get { return this.error; }
+		}
+		public override bool IsCustomErrorEnabled
+		{
+			get { return this.customErrors; }
+		}
+		public override bool IsDebuggingEnabled
+		{
+			get { return this.debuggingEnabled; }
+		}
+		public override bool IsPostNotification
+		{
+			get { return this.postNotification; }
+		}
+		public override DateTime Timestamp
+		{
+			get { return this.timestamp; }
+		}
+		public override Exception[] AllErrors
+		{
+			get { return this.allErrors; }
+		}
+		public override bool SkipAuthorization
+		{
+			get { return this.skipAuthorization; }
+			set { }
+		}
+		public override IPrincipal User
+		{
+			get { return this.user; }
+			set { }
+		}
+		public override HttpRequestBase Request
+		{
+			get { return this.request; }
+		}
+
+		public HttpContextClone(HttpContextBase context)
+		{
+			this.error = context.Error;
+			this.allErrors = context.AllErrors;
+			this.debuggingEnabled = context.IsDebuggingEnabled;
+			this.customErrors = context.IsCustomErrorEnabled;
+			this.postNotification = context.IsPostNotification;
+			this.timestamp = context.Timestamp;
+			this.skipAuthorization = context.SkipAuthorization;
+			this.user = context.User;
+
+			request = new HttpRequestClone(context.Request);
+		}
+
+		private readonly HttpRequestBase request;
+		private readonly Exception error;
+		private readonly Exception[] allErrors;
+		private readonly bool postNotification;
+		private readonly bool debuggingEnabled;
+		private readonly bool customErrors;
+		private readonly DateTime timestamp;
+		private readonly bool skipAuthorization;
+		private readonly IPrincipal user;
+	}
+
+	internal class HttpRequestClone : HttpRequestBase
+	{
+		public override string[] AcceptTypes
+		{
+			get { return this.acceptTypes; }
+		}
+		public override string AnonymousID
+		{
+			get { return this.anonymousId; }
+		}
+		public override string ApplicationPath
+		{
+			get { return this.applicationPath; }
+		}
+		public override string AppRelativeCurrentExecutionFilePath
+		{
+			get { return this.appRelativeCurrentExecutionFilePath; }
+		}
+		public override Encoding ContentEncoding
+		{
+			get { return this.contentEncoding; }
+			set { }
+		}
+		public override int ContentLength
+		{
+			get { return this.contentLength; }
+		}
+		public override string ContentType
+		{
+			get { return this.contentType; }
+		}
+		public override string CurrentExecutionFilePath
+		{
+			get { return this.currentExecutionFilePath; }
+		}
+		public override string HttpMethod
+		{
+			get { return this.httpMethod; }
+		}
+		public override bool IsAuthenticated
+		{
+			get { return this.isAuthenticated; }
+		}
+		public override bool IsLocal
+		{
+			get { return this.isLocal; }
+		}
+		public override bool IsSecureConnection
+		{
+			get { return this.isSecureConnection; }
+		}
+		public override string Path
+		{
+			get { return this.path; }
+		}
+		public override string PathInfo
+		{
+			get { return this.pathInfo; }
+		}
+		public override string PhysicalApplicationPath
+		{
+			get { return this.physicalApplicationPath; }
+		}
+		public override string PhysicalPath
+		{
+			get { return this.physicalPath; }
+		}
+		public override string RawUrl
+		{
+			get { return this.rawUrl; }
+		}
+		public override string RequestType
+		{
+			get { return this.requestType; }
+		}
+		public override int TotalBytes
+		{
+			get { return this.totalBytes; }
+		}
+		public override Uri Url
+		{
+			get { return this.url; }
+		}
+		public override Uri UrlReferrer
+		{
+			get { return this.urlReferrer; }
+		}
+		public override string UserHostAddress
+		{
+			get { return this.userHostAddress; }
+		}
+		public override string UserHostName
+		{
+			get { return this.userHostName; }
+		}
+		public override string UserAgent
+		{
+			get { return this.userAgent; }
+		}
+		public override string[] UserLanguages
+		{
+			get { return this.userLanguages; }
+		}
+
+		public override HttpCookieCollection Cookies
+		{
+			get { return this.cookies; }
+		}
+		public override NameValueCollection Headers
+		{
+			get { return this.headers; }
+		}
+		public override NameValueCollection Form
+		{
+			get { return this.form; }
+		}
+		public override NameValueCollection QueryString
+		{
+			get { return this.queryString; }
+		}
+		public override NameValueCollection ServerVariables
+		{
+			get { return this.serverVariables; }
+		}
+
+		public HttpRequestClone(HttpRequestBase request)
+		{
+			this.acceptTypes = request.AcceptTypes;
+			this.anonymousId = request.AnonymousID;
+			this.applicationPath = request.ApplicationPath;
+			this.appRelativeCurrentExecutionFilePath = request.AppRelativeCurrentExecutionFilePath;
+			this.contentEncoding = request.ContentEncoding;
+			this.contentLength = request.ContentLength;
+			this.contentType = request.ContentType;
+			this.currentExecutionFilePath = request.CurrentExecutionFilePath;
+			this.isSecureConnection = request.IsSecureConnection;
+			this.isLocal = request.IsLocal;
+			this.isAuthenticated = request.IsAuthenticated;
+			this.httpMethod = request.HttpMethod;
+			this.path = request.Path;
+			this.pathInfo = request.PathInfo;
+			this.physicalPath = request.PhysicalPath;
+			this.physicalApplicationPath = request.PhysicalApplicationPath;
+			this.rawUrl = request.RawUrl;
+			this.requestType = request.RequestType;
+			this.totalBytes = request.TotalBytes;
+			this.url = request.Url;
+			this.urlReferrer = request.UrlReferrer;
+			this.userHostAddress = request.UserHostAddress;
+			this.userHostName = request.UserHostName;
+			this.userAgent = request.UserAgent;
+			this.userLanguages = request.UserLanguages;
+
+			this.headers = new NameValueCollection(request.Headers);
+			this.form = new NameValueCollection(request.Form);
+			this.queryString = new NameValueCollection(request.QueryString);
+			this.serverVariables = new NameValueCollection(request.ServerVariables);
+
+			this.cookies = new HttpCookieCollection();
+			foreach (HttpCookie cookie in request.Cookies)
+				this.cookies.Add(cookie);
+		}
+
+		private readonly string userAgent;
+		private readonly string[] acceptTypes;
+		private readonly string anonymousId;
+		private readonly string applicationPath;
+		private readonly string appRelativeCurrentExecutionFilePath;
+		private readonly Encoding contentEncoding;
+		private readonly int contentLength;
+		private readonly string contentType;
+		private readonly string currentExecutionFilePath;
+		private readonly bool isSecureConnection;
+		private readonly bool isLocal;
+		private readonly bool isAuthenticated;
+		private readonly string httpMethod;
+		private readonly string path;
+		private readonly string pathInfo;
+		private readonly string physicalApplicationPath;
+		private readonly string physicalPath;
+		private readonly string rawUrl;
+		private readonly string requestType;
+		private readonly int totalBytes;
+		private readonly Uri url;
+		private readonly Uri urlReferrer;
+		private readonly string userHostAddress;
+		private readonly string userHostName;
+		private readonly string[] userLanguages;
+		private readonly HttpCookieCollection cookies;
+		private readonly NameValueCollection headers;
+		private readonly NameValueCollection queryString;
+		private readonly NameValueCollection form;
+		private readonly NameValueCollection serverVariables;
+	}
+}
---
+++ src/tests/NanoMessageBus.UnitTests/Channels/HttpRequestAuditorExtensionsTests.cs
@@ -0,0 +1,222 @@
+﻿#pragma warning disable 169
+// ReSharper disable InconsistentNaming
+
+namespace NanoMessageBus.Channels
+{
+	using System;
+	using System.Collections.Specialized;
+	using System.Linq;
+	using System.Security.Principal;
+	using System.Text;
+	using System.Web;
+	using Machine.Specifications;
+	using Moq;
+	using It = Machine.Specifications.It;
+
+	[Subject(typeof(HttpRequestAuditorExtensions))]
+	public class when_cloning_an_http_context
+	{
+		Establish context = () =>
+		{
+			var mockContext = new Mock<HttpContextBase>();
+			var mockRequest = new Mock<HttpRequestBase>();
+
+			mockContext.Setup(x => x.Request).Returns(mockRequest.Object);
+			mockContext.Setup(x => x.User).Returns(new Mock<IPrincipal>().Object);
+
+			mockContext.Setup(x => x.Error).Returns(error);
+			mockContext.Setup(x => x.AllErrors).Returns(new[] { error });
+			mockContext.Setup(x => x.IsDebuggingEnabled).Returns(true);
+			mockContext.Setup(x => x.IsCustomErrorEnabled).Returns(true);
+			mockContext.Setup(x => x.IsPostNotification).Returns(true);
+			mockContext.Setup(x => x.SkipAuthorization).Returns(true);
+			mockContext.Setup(x => x.Timestamp).Returns(SystemTime.UtcNow);
+
+			mockRequest.Setup(x => x.UserAgent).Returns("user agent");
+			mockRequest.Setup(x => x.AcceptTypes).Returns(new[] { "gzip" });
+			mockRequest.Setup(x => x.AnonymousID).Returns("anonymous id");
+			mockRequest.Setup(x => x.ApplicationPath).Returns("application path");
+			mockRequest.Setup(x => x.AppRelativeCurrentExecutionFilePath).Returns("app relative path");
+			mockRequest.Setup(x => x.ContentEncoding).Returns(Encoding.UTF32);
+			mockRequest.Setup(x => x.ContentLength).Returns(42);
+			mockRequest.Setup(x => x.ContentType).Returns("custom/content");
+			mockRequest.Setup(x => x.FilePath).Returns("file/path/here");
+			mockRequest.Setup(x => x.HttpMethod).Returns("custom method");
+			mockRequest.Setup(x => x.IsAuthenticated).Returns(true);
+			mockRequest.Setup(x => x.IsLocal).Returns(true);
+			mockRequest.Setup(x => x.IsSecureConnection).Returns(true);
+			mockRequest.Setup(x => x.Path).Returns("standard/path");
+			mockRequest.Setup(x => x.PathInfo).Returns("standard/path/info");
+			mockRequest.Setup(x => x.PhysicalApplicationPath).Returns("physical app path");
+			mockRequest.Setup(x => x.PhysicalPath).Returns("physical/path");
+			mockRequest.Setup(x => x.RawUrl).Returns("raw/url?value");
+			mockRequest.Setup(x => x.RequestType).Returns("request-type");
+			mockRequest.Setup(x => x.TotalBytes).Returns(42 + 42);
+			mockRequest.Setup(x => x.Url).Returns(new Uri("http://www.google.com/"));
+			mockRequest.Setup(x => x.UrlReferrer).Returns(new Uri("http://www.google.com/referer"));
+			mockRequest.Setup(x => x.UserHostAddress).Returns("host-address");
+			mockRequest.Setup(x => x.UserHostName).Returns("host-name");
+			mockRequest.Setup(x => x.UserLanguages).Returns(new[] { "en-us" });
+
+			mockRequest.Setup(x => x.Headers).Returns(new NameValueCollection());
+			mockRequest.Setup(x => x.Form).Returns(new NameValueCollection());
+			mockRequest.Setup(x => x.QueryString).Returns(new NameValueCollection());
+			mockRequest.Setup(x => x.ServerVariables).Returns(new NameValueCollection());
+			mockRequest.Setup(x => x.Cookies).Returns(new HttpCookieCollection());
+
+			original = mockContext.Object;
+		};
+
+		Because of = () =>
+			clone = original.Clone();
+
+		It should_return_a_object = () =>
+			clone.ShouldNotBeNull();
+
+		It should_not_return_the_same_instance_as_provided = () =>
+			ReferenceEquals(clone, original).ShouldBeFalse();
+
+		It should_populate_the_request = () =>
+			clone.Request.ShouldNotBeNull();
+
+		It should_not_return_the_same_instance_of_the_request_provided = () =>
+			ReferenceEquals(clone.Request, original.Request).ShouldBeFalse();
+
+		It should_return_a_reference_to_any_error_on_the_original = () =>
+			clone.Error.ShouldEqual(original.Error);
+
+		It should_return_a_reference_to_all_on_the_original = () =>
+			clone.AllErrors.SequenceEqual(original.AllErrors).ShouldBeTrue();
+
+		It should_indicate_if_debugging_is_enabled = () =>
+			clone.IsDebuggingEnabled.ShouldEqual(original.IsDebuggingEnabled);
+
+		It should_indicate_if_custom_errors_are_enabled = () =>
+			clone.IsCustomErrorEnabled.ShouldEqual(original.IsCustomErrorEnabled);
+
+		It should_indicate_if_the_request_is_a_post = () =>
+			clone.IsPostNotification.ShouldEqual(original.IsPostNotification);
+
+		It should_indicate_if_authorization_has_been_skipped = () =>
+			clone.SkipAuthorization.ShouldEqual(original.SkipAuthorization);
+
+		It should_clone_the_context_timestamp = () =>
+			clone.Timestamp.ShouldEqual(original.Timestamp);
+
+		It should_return_a_reference_to_the_current_principal = () =>
+			clone.User.ShouldEqual(original.User);
+
+		It should_return_the_same_user_agent = () =>
+			clone.Request.UserAgent.ShouldEqual(original.Request.UserAgent);
+
+		It should_return_the_same_accept_types = () =>
+			clone.Request.AcceptTypes.ShouldEqual(original.Request.AcceptTypes);
+
+		It should_return_the_same_anonymous_id = () =>
+			clone.Request.AnonymousID.ShouldEqual(original.Request.AnonymousID);
+
+		It should_return_the_same_app_path = () =>
+			clone.Request.ApplicationPath.ShouldEqual(original.Request.ApplicationPath);
+
+		It should_return_the_same_app_relative_path = () =>
+			clone.Request.AppRelativeCurrentExecutionFilePath.ShouldEqual(
+			original.Request.AppRelativeCurrentExecutionFilePath);
+
+		It should_return_the_same_content_encoding = () =>
+			clone.Request.ContentEncoding.ShouldEqual(original.Request.ContentEncoding);
+
+		It should_return_the_same_content_length = () =>
+			clone.Request.ContentLength.ShouldEqual(original.Request.ContentLength);
+
+		It should_return_the_same_content_type = () =>
+			clone.Request.ContentType.ShouldEqual(original.Request.ContentType);
+
+		It should_return_the_same_execution_path = () =>
+			clone.Request.CurrentExecutionFilePath.ShouldEqual(original.Request.CurrentExecutionFilePath);
+
+		It should_return_the_same_http_method = () =>
+			clone.Request.HttpMethod.ShouldEqual(original.Request.HttpMethod);
+
+		It should_return_the_same_is_authenticated_property = () =>
+			clone.Request.IsAuthenticated.ShouldEqual(original.Request.IsAuthenticated);
+
+		It should_return_the_same_is_local_property = () =>
+			clone.Request.IsLocal.ShouldEqual(original.Request.IsLocal);
+
+		It should_return_the_same_is_secure_connection_property = () =>
+			clone.Request.IsSecureConnection.ShouldEqual(original.Request.IsSecureConnection);
+
+		It should_return_the_same_path = () =>
+			clone.Request.Path.ShouldEqual(original.Request.Path);
+
+		It should_return_the_same_path_info = () =>
+			clone.Request.PathInfo.ShouldEqual(original.Request.PathInfo);
+
+		It should_return_the_same_physical_app_path = () =>
+			clone.Request.PhysicalApplicationPath.ShouldEqual(original.Request.PhysicalApplicationPath);
+
+		It should_return_the_same_physical_path = () =>
+			clone.Request.PhysicalPath.ShouldEqual(original.Request.PhysicalPath);
+
+		It should_return_the_same_raw_url = () =>
+			clone.Request.RawUrl.ShouldEqual(original.Request.RawUrl);
+
+		It should_return_the_same_request_type = () =>
+			clone.Request.RequestType.ShouldEqual(original.Request.RequestType);
+
+		It should_return_the_same_total_bytes_value = () =>
+			clone.Request.TotalBytes.ShouldEqual(original.Request.TotalBytes);
+
+		It should_return_the_same_url = () =>
+			clone.Request.Url.ShouldEqual(original.Request.Url);
+
+		It should_return_the_same_referring_url = () =>
+			clone.Request.UrlReferrer.ShouldEqual(original.Request.UrlReferrer);
+
+		It should_return_the_same_user_host_address = () =>
+			clone.Request.UserHostAddress.ShouldEqual(original.Request.UserHostAddress);
+
+		It should_return_the_same_user_host_name = () =>
+			clone.Request.UserHostName.ShouldEqual(original.Request.UserHostName);
+
+		It should_return_the_same_user_languages = () =>
+			clone.Request.UserLanguages.ShouldEqual(original.Request.UserLanguages);
+
+		It should_return_the_cookie_collection = () =>
+			clone.Request.Cookies.ShouldNotBeNull();
+
+		It should_return_the_clone_the_cookies = () =>
+			ReferenceEquals(clone.Request.Cookies, original.Request.Cookies).ShouldBeFalse();
+
+		It should_return_the_header_collection = () =>
+			clone.Request.Headers.ShouldNotBeNull();
+
+		It should_return_the_clone_the_headers = () =>
+			ReferenceEquals(clone.Request.Headers, original.Request.Headers).ShouldBeFalse();
+
+		It should_return_the_form_collection = () =>
+			clone.Request.Form.ShouldNotBeNull();
+
+		It should_return_the_clone_the_form_data = () =>
+			ReferenceEquals(clone.Request.Form, original.Request.Form).ShouldBeFalse();
+
+		It should_return_the_query_string_collection = () =>
+			clone.Request.QueryString.ShouldNotBeNull();
+
+		It should_return_the_clone_the_query_string_data = () =>
+			ReferenceEquals(clone.Request.QueryString, original.Request.QueryString).ShouldBeFalse();
+
+		It should_return_the_server_variables_collection = () =>
+			clone.Request.ServerVariables.ShouldNotBeNull();
+
+		It should_return_the_clone_the_server_variables_data = () =>
+			ReferenceEquals(clone.Request.ServerVariables, original.Request.ServerVariables).ShouldBeFalse();
+
+		static HttpContextBase original;
+		static HttpContextBase clone;
+		static readonly Exception error = new Exception();
+	}
+}
+
+// ReSharper enable InconsistentNaming
+#pragma warning restore 169
