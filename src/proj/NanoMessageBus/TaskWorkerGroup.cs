516063857d1ba5682ae0f7c72ec3997a185e54fc
Oliver, Jonathan <joliver@localhost>
2012-07-04 22:34:00 -0600

    Added TODO item related to GitHub issue.

--- src/proj/NanoMessageBus/TaskWorkerGroup.cs
+++ src/proj/NanoMessageBus/TaskWorkerGroup.cs
@@ -130,7 +130,7 @@
 			try
 			{
 				foreach (var item in this.workItems.GetConsumingEnumerable(token))
-					item(worker);
+					item(worker); // TODO: #60 if this operation isn't completely successful, the item should be re-enqueued
 			}
 			catch (OperationCanceledException)
 			{
