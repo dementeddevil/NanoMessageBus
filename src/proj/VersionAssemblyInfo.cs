a6e899d1e895b3ed0e5f72fa0b2220ebfddda44a
Oliver, Jonathan <joliver@localhost>
2012-07-05 10:10:09 -0600

    Bumped version to 2.0.21.

--- src/proj/VersionAssemblyInfo.cs
+++ src/proj/VersionAssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Reflection;
 //------------------------------------------------------------------------------
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.20.0")]
-[assembly: AssemblyInformationalVersion("2.0 Release Build 20 (2012-07-04)")]
+[assembly: AssemblyFileVersion("2.0.21.0")]
+[assembly: AssemblyInformationalVersion("2.0 Release Build 21 (2012-07-05)")]
