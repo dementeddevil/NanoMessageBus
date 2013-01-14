532f1ce5ac1515a1202133965e5caa057828e550
Oliver, Jonathan <joliver@localhost>
2013-01-14 10:24:47 -0700

    Bumped version in preparation for .28 release.

--- purge.cmd
+++ purge.cmd
@@ -1,9 +1,9 @@
 echo off
-:: run this when 2.0.26 is obsolete
-src\.nuget\NuGet.exe delete NanoMessageBus 2.0.26 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.RabbitMQ 2.0.26 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.Autofac 2.0.26 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.Json.NET 2.0.26 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net 2.0.26 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net2 2.0.26 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.NLog 2.0.26 -NoPrompt
+:: run this when 2.0.27 is obsolete
+src\.nuget\NuGet.exe delete NanoMessageBus 2.0.27 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.RabbitMQ 2.0.27 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.Autofac 2.0.27 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.Json.NET 2.0.27 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net 2.0.27 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net2 2.0.27 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.NLog 2.0.27 -NoPrompt
