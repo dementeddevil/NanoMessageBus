e5317c4fea6b178d2ecbef57ae668f1ef0efefcc
Oliver, Jonathan <joliver@localhost>
2013-01-14 15:42:13 -0700

    Bumped version to 2.0.29.

--- purge.cmd
+++ purge.cmd
@@ -1,9 +1,9 @@
 echo off
-:: run this when 2.0.27 is obsolete
-src\.nuget\NuGet.exe delete NanoMessageBus 2.0.27 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.RabbitMQ 2.0.27 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.Autofac 2.0.27 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.Json.NET 2.0.27 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net 2.0.27 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net2 2.0.27 -NoPrompt
-src\.nuget\NuGet.exe delete NanoMessageBus.NLog 2.0.27 -NoPrompt
+:: run this when 2.0.28 is obsolete
+src\.nuget\NuGet.exe delete NanoMessageBus 2.0.28 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.RabbitMQ 2.0.28 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.Autofac 2.0.28 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.Json.NET 2.0.28 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net 2.0.28 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net2 2.0.28 -NoPrompt
+src\.nuget\NuGet.exe delete NanoMessageBus.NLog 2.0.28 -NoPrompt
--- src/proj/VersionAssemblyInfo.cs
+++ src/proj/VersionAssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Reflection;
 //------------------------------------------------------------------------------
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.28.0")]
+[assembly: AssemblyFileVersion("2.0.29.0")]
 [assembly: AssemblyInformationalVersion("2.0 Release Build 27 (2013-01-14)")]
