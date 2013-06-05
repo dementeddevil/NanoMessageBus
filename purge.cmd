ffae483ff045d7027198d8060eee67fda92610ce
Oliver, Jonathan <jonathan@localhost>
2013-06-05 15:12:30 -0600

    Marking NuGet packages with new version.

--- purge.cmd
+++ purge.cmd
@@ -1,9 +1,9 @@
 echo off
-:: run this when 2.0.36 is obsolete
-src\.nuget\NuGet.exe delete NanoMessageBus 2.0.36 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.RabbitMQ 2.0.36 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.Autofac 2.0.36 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.Json.NET 2.0.36 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net 2.0.36 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net2 2.0.36 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.NLog 2.0.36 -NonInteractive
+:: run this when 2.0.37 is obsolete
+src\.nuget\NuGet.exe delete NanoMessageBus 2.0.37 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.RabbitMQ 2.0.37 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.Autofac 2.0.37 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.Json.NET 2.0.37 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net 2.0.37 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net2 2.0.37 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.NLog 2.0.37 -NonInteractive
