97b2f3a3da6c0ec7a3b1270309cbbc56ae297b52
Oliver, Jonathan <git@jonathanoliver.com>
2014-01-16 19:58:45 -0700

    Bumped version.

--- purge.cmd
+++ purge.cmd
@@ -1,8 +1,8 @@
 echo off
-:: run this when 2.0.45 is obsolete
-src\.nuget\NuGet.exe delete NanoMessageBus 2.0.45 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.RabbitMQ 2.0.45 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.Autofac 2.0.45 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.Json.NET 2.0.45 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net 2.0.45 -NonInteractive
-src\.nuget\NuGet.exe delete NanoMessageBus.NLog 2.0.45 -NonInteractive
+:: run this when 2.0.46 is obsolete
+src\.nuget\NuGet.exe delete NanoMessageBus 2.0.46 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.RabbitMQ 2.0.46 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.Autofac 2.0.46 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.Json.NET 2.0.46 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.Log4Net 2.0.46 -NonInteractive
+src\.nuget\NuGet.exe delete NanoMessageBus.NLog 2.0.46 -NonInteractive
--- src/proj/VersionAssemblyInfo.cs
+++ src/proj/VersionAssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Reflection;
 //------------------------------------------------------------------------------
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.45.0")]
-[assembly: AssemblyInformationalVersion("2.0 Release Build 45 (2014-01-17)")]
+[assembly: AssemblyFileVersion("2.0.46.0")]
+[assembly: AssemblyInformationalVersion("2.0 Release Build 46 (2014-01-17)")]
