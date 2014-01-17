8df1b97edfc7c5d2308f4e15202cf443dd86c7be
Oliver, Jonathan <git@jonathanoliver.com>
2014-01-16 19:35:00 -0700

    Added cloud auditing to the mix.

---
+++ src/proj/NanoMessageBus/Channels/CloudAuditor.cs
@@ -0,0 +1,54 @@
+﻿namespace NanoMessageBus.Channels
+{
+	using System;
+
+	public class CloudAuditor : IMessageAuditor
+	{
+		public void AuditReceive(IDeliveryContext delivery)
+		{
+			// no op
+		}
+		public void AuditSend(ChannelEnvelope envelope, IDeliveryContext delivery)
+		{
+			if (!this.cloud)
+				return;
+
+			if (envelope == null)
+				throw new ArgumentNullException("envelope");
+
+			if (delivery != null && delivery.CurrentMessage == envelope.Message)
+				return;
+
+			var message = envelope.Message;
+			if (message == envelope.State)
+				return;
+
+			var headers = message.Headers;
+			headers[FacilityNameHeader] = FacilityName;
+			headers[MachineIdHeader] = MachineId;
+		}
+
+		public CloudAuditor()
+		{
+			this.cloud = !string.IsNullOrEmpty(FacilityName) && !string.IsNullOrEmpty(MachineId);
+		}
+		~CloudAuditor()
+		{
+		}
+		public void Dispose()
+		{
+			this.Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		protected virtual void Dispose(bool disposing)
+		{
+			// no op
+		}
+
+		private const string FacilityNameHeader = "x-cloud-facility";
+		private const string MachineIdHeader = "x-cloud-machine";
+		private static readonly string FacilityName = CloudDetection.DetectFacility() ?? string.Empty;
+		private static readonly string MachineId = CloudDetection.DetectMachineId() ?? string.Empty;
+		private readonly bool cloud;
+	}
+}
---
+++ src/proj/NanoMessageBus/Channels/CloudDetection.cs
@@ -0,0 +1,28 @@
+﻿namespace NanoMessageBus.Channels
+{
+	using System.Net;
+
+	internal static class CloudDetection
+	{
+		public static string DetectFacility()
+		{
+			return DownloadMetadata("placement/availability-zone");
+		}
+		public static string DetectMachineId()
+		{
+			return DownloadMetadata("instance-id");
+		}
+		private static string DownloadMetadata(string name)
+		{
+			try
+			{
+				using (var client = new WebClient())
+					return client.DownloadString("http://169.254.169.254/latest/meta-data/" + name);
+			}
+			catch
+			{
+				return null;
+			}
+		}
+	}
+}
